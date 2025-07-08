68f94a3d5c7b1747e01e58bc97a56077e114741b
cjn404 <moi404@naver.com>
2025-07-08 17:58:44 +0900

    Add No1.java - 두 수의 차 구하기

---
+++ src/main/java/intro/No1.java
@@ -0,0 +1,21 @@
+package intro;
+
+/*
+📌 No1. 두 수의 차 구하기 (답안 2가지)
+문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120803
+
+📝 문제 설명:
+정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return 하도록 solution 함수를 완성해주세요.
+*/
+
+
+public class No1 {
+    public int solution1(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public int solution2(int num1, int num2) {
+        int sub = num1 - num2;
+        return sub;
+    }
+}

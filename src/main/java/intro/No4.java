0588ab28190a852af1bda01f4a9249dc9e900584
cjn404 <moi404@naver.com>
2025-07-11 09:36:36 +0900

    Add No4.java - 나이 출력

---
+++ src/main/java/intro/No4.java
@@ -0,0 +1,23 @@
+package intro;
+
+/*
+📌 No4. 나이 출력
+문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120820
+
+📝 문제 설명:
+머쓱이는 선생님이 몇 년도에 태어났는지 궁금해졌습니다. 2022년 기준 선생님의 나이 age가 주어질 때, 선생님의 출생 연도를 return 하는 solution 함수를 완성해주세요
+
+🚫 제한 사항:
+0 < age ≤ 120
+나이는 태어난 연도에 1살이며 매년 1월 1일마다 1살씩 증가합니다.
+*/
+
+public class No4 {
+
+    public int solution(int age) {
+        if (0 < age && age <= 120) {
+            return 2022 - age + 1;
+        }
+        return 0;
+    }
+}

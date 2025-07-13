c986265594b398867821fc9b8becef950402b292
cjn404 <moi404@naver.com>
2025-07-13 21:32:28 +0900

    feat: add No7 sql 아픈 동물 찾기

---
+++ src/main/sql/intro/No7.sql
@@ -0,0 +1,13 @@
+/*
+📌 No7. 아픈 동물 찾기
+문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59036
+
+📝 문제 설명:
+NIMAL_INS 테이블에서 ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE는 각각 동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부를 나타냅니다.
+동물 보호소에 들어온 동물 중 아픈 동물의 아이디와 이름을 조회하는 SQL 문을 작성해주세요. 이때 결과는 아이디 순으로 조회해주세요.
+*/
+
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE INTAKE_CONDITION = 'Sick'
+ORDER BY ANIMAL_ID;

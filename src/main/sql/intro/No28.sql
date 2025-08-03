696a17818e7b79a3e47022ccdf8ddae250b64ba0
cjn404 <moi404@naver.com>
2025-08-03 10:01:51 +0900

    feat: add No28 sql 12세 이하인 여자 환자 목록 출력하기

---
+++ src/main/sql/intro/No28.sql
@@ -0,0 +1,35 @@
+/*
+📌 No28. 12세 이하인 여자 환자 목록 출력하기 (답안 2가지)
+문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/132201
+
+📝 문제 설명:
+다음은 종합병원에 등록된 환자정보를 담은 PATIENT 테이블입니다. PATIENT 테이블은 다음과 같으며 PT_NO, PT_NAME, GEND_CD, AGE, TLNO는 각각 환자번호, 환자이름, 성별코드, 나이, 전화번호를 의미합니다.
+PATIENT 테이블에서 12세 이하인 여자환자의 환자이름, 환자번호, 성별코드, 나이, 전화번호를 조회하는 SQL문을 작성해주세요. 이때 전화번호가 없는 경우, 'NONE'으로 출력시켜 주시고 결과는 나이를 기준으로 내림차순 정렬하고, 나이 같다면 환자이름을 기준으로 오름차순 정렬해주세요.
+*/
+
+-- 1. CASE WHEN
+SELECT PT_NAME,
+       PT_NO,
+       GEND_CD,
+       AGE,
+       CASE
+           WHEN TLNO IS NULL THEN 'NONE'
+           ELSE TLNO
+       END AS TLNO
+FROM PATIENT
+WHERE GEND_CD = 'W'
+  AND AGE <= 12
+ORDER BY AGE DESC,
+         PT_NAME ASC;
+
+-- 2. COALESCE
+SELECT PT_NAME,
+       PT_NO,
+       GEND_CD,
+       AGE,
+       COALESCE(TLNO, 'NONE') AS TLNO
+FROM PATIENT
+WHERE GEND_CD = 'W'
+  AND AGE <= 12
+ORDER BY AGE DESC,
+         PT_NAME ASC;

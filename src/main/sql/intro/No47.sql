c50e3e289f703cc62b10cda2a9361e6f0b3c91db
cjn404 <moi404@naver.com>
2025-09-17 09:25:42 +0900

    feat: add No47 sql 모든 레코드 조회하기

---
+++ src/main/sql/intro/No47.sql
@@ -0,0 +1,11 @@
+/*
+📌 No47. 모든 레코드 조회하기
+문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59034
+
+📝 문제 설명:
+동물 보호소에 들어온 모든 동물의 정보를 ANIMAL_ID순으로 조회하는 SQL문을 작성해주세요.
+*/
+
+SELECT *
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;

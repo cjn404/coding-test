98b10174a7cbd1ac5fbf9e57c9b7bd0760cc7d18
cjn404 <moi404@naver.com>
2026-03-03 01:09:11 +0900

    solve: programmers Lv. 0 두 수의 합 구하기(No. 120802)

---
+++ src/main/java/programmers/java/lv0/no120802/No120802.java
@@ -0,0 +1,19 @@
+package programmers.java.lv0.no120802;
+
+public class No120802 {
+    public int solution1(int num1, int num2) {
+        return method1(num1, num2);
+    }
+
+    // 방법 1
+    private int method1(int num1, int num2) {
+        int sum;
+        sum = num1 + num2;
+        return sum;
+    }
+
+    // 방법 2
+    public int solution2(int num1, int num2) {
+        return num1 + num2;
+    }
+}

4da54d6bdafa87f5d8b002a7e44d4d57d1dee58c
cjn404 <moi404@naver.com>
2026-03-03 18:25:08 +0900

    solve: programmers Lv. 0 두 수의 차 구하기(No. 120803)

---
+++ src/main/java/programmers/java/lv0/no120803/No120803.java
@@ -0,0 +1,15 @@
+package programmers.java.lv0.no120803;
+
+public class No120803 {
+    // 방법 1
+    public int solution1(int num1, int num2) {
+        int sub;
+        sub = num1 - num2;
+        return sub;
+    }
+
+    // 방법 2
+    public int solution2(int num1, int num2) {
+        return num1 - num2;
+    }
+}
---
+++ src/main/java/programmers/java/lv0/no120803/README.md
@@ -0,0 +1,10 @@
+# Lv. 0 두 수의 차 구하기(No. 120803)
+
+[🔗 문제 바로가기](https://school.programmers.co.kr/learn/courses/30/lessons/120803)
+
+## 문제 설명
+정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+
+## 제한 사항
+- 50000 ≤ num1 ≤ 50000
+- 50000 ≤ num2 ≤ 50000

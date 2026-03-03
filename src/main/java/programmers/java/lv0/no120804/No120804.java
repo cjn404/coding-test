abb27e92875cb5e78d41a58366ff996ff77094df
cjn404 <moi404@naver.com>
2026-03-03 18:58:31 +0900

    solve: programmers Lv. 0 두 수의 곱 구하기(No. 120804)

---
+++ src/main/java/programmers/java/lv0/no120804/No120804.java
@@ -0,0 +1,15 @@
+package programmers.java.lv0.no120804;
+
+public class No120804 {
+    // 방법 1
+    public int solution1(int num1, int num2) {
+        int product;
+        product = num1 * num2;
+        return product;
+    }
+
+    // 방법 2
+    public int solution2(int num1, int num2) {
+        return num1 * num2;
+    }
+}

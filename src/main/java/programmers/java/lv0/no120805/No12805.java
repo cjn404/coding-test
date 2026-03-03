e8f6921201c7c15e6a8d3f5d01809fd159c4d4d1
cjn404 <moi404@naver.com>
2026-03-03 19:12:56 +0900

    solve: programmers Lv. 0 몫 구하기(No. 120805)

---
+++ src/main/java/programmers/java/lv0/no120805/No12805.java
@@ -0,0 +1,15 @@
+package programmers.java.lv0.no120805;
+
+public class No12805 {
+    // 방법 1
+    public int solution1(int num1, int num2) {
+        int quotient;
+        quotient = num1 / num2;
+        return quotient;
+    }
+
+    // 방법 2
+    public int solution2(int num1, int num2) {
+        return num1 / num2;
+    }
+}
---
+++ src/main/java/programmers/java/lv0/no120805/README.md
@@ -0,0 +1,10 @@
+# Lv. 0 몫 구하기(No. 120805)
+
+[🔗 문제 바로가기](https://school.programmers.co.kr/learn/courses/30/lessons/120805)
+
+## 문제 설명
+정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+
+## 제한 사항
+- 0 < num1 ≤ 100
+- 0 < num2 ≤ 100
